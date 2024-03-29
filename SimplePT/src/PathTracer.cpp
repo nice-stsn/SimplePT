45fd3ee4df8eb854da267b360dbce2caec932d05
ChengjianTao <1305393329@qq.com>
2024-03-29 10:56:21 +0800

    Finish Cornell-box

--- SimplePT/src/PathTracer.cpp
+++ SimplePT/src/PathTracer.cpp
@@ -46,19 +46,23 @@ void PathTracer::Render(int num_samples_per_pixel)
 	const int height = m_camera.GetHeight();
 
 	int linecnt = 0;
+#ifdef OMP_PT
 	#pragma omp parallel for 
+#endif // OMP_PT
 	for (int j = 0; j < height; ++j)
 	{
 		if (linecnt % 50 == 1)
 			std::clog << "\rline: " << linecnt << '/' << height - 1 << "     " << std::endl;
 		for (int i = 0; i < width; ++i)
 		{
+#ifdef DEBUG_PT
 			/* debug: get processing image for debuggging */
 			if (i == debug_pixel_x && j == debug_pixel_y)
 			{
 				m_WritePixelColor(i, j, Color3(255u, 0u, 0u)); // red pixel
 				stbi_write_png("image/debug.png", width, height, CHANNEL_NUM, m_frame_buffer.get(), width * CHANNEL_NUM);
 			}
+#endif // DEBUG_PT
 
 			Vector3 pixel_color_radiance(0.0, 0.0, 0.0);
 			for (int sppcnt = 0; sppcnt < num_samples_per_pixel; ++sppcnt)
@@ -67,23 +71,16 @@ void PathTracer::Render(int num_samples_per_pixel)
 				pixel_color_radiance +=  m_RayRadiance(eye_ray) / num_samples_per_pixel;
 			}
 			m_WritePixelRadiance(i, j, pixel_color_radiance);
-
-			///* debug: get processing image for debuggging */
-			//if (i == debug_pixel_x && j == debug_pixel_y)
-			//{
-			//	m_WritePixelColor(i, j, Color3(255u, 0u, 0u)); // red pixel
-			//	stbi_write_png("image/debug.png", width, height, CHANNEL_NUM, m_frame_buffer.get(), width * CHANNEL_NUM);
-			//}
 		}
+#ifdef OMP_PT
 		#pragma omp atomic
+#endif // OMP_PT
 		linecnt++;
 	}
 
 	// if CHANNEL_NUM is 4, you can use alpha channel in png
 	stbi_write_png("image/stbpng.png", width, height, CHANNEL_NUM, m_frame_buffer.get(), width * CHANNEL_NUM);
 
-	//// You have to use 3 comp for complete jpg file. If not, the image will be grayscale or nothing.
-	//stbi_write_jpg("image/stbjpg3.jpg", width, height, 3, m_frame_buffer.get(), 100);
 }
 
 // Evaluate Rendering Equation
@@ -102,10 +99,6 @@ Vector3 PathTracer::m_RayRadiance(const Ray& ray) const
 	if (!m_scene.HitHappened(ray, hit_record))
 		return Vector3(); // defualt radiance (black)
 
-	//// debug: normal shading
-	//Vector3 normal = hit_record.m_hit_unit_normal;
-	//return Vector3(std::abs(normal.m_x), std::abs(normal.m_y), std::abs(normal.m_z));
-
 	/* first term: Le(x1 -> x0) */
 	if (hit_record.m_material.HasEmission())
 	{
@@ -114,27 +107,19 @@ Vector3 PathTracer::m_RayRadiance(const Ray& ray) const
 		return out_radiance; // eye ray hit light
 	}
 
+	// surface info
+	Vector3 kd = hit_record.m_material.GetKd(); 
+	Vector3 BRDF = kd / SimplePT::PI; 
+
 	/* second term: \int le(x2 -> x1) * f(x2 -> x1 -> x0) * cos(\theta) dw */
-	// random_point_on_light = PdfAreaLight(); // uniform p = 1/a && 1/p = a
 	// direct = brdf * (n * w) * (n' * w') * light_emmit / r ^ 2 / p; (integral on da)
-	Vector3 direct_illum_radiance;
-	double pdf_all_light;
 	HitRecord light_surface_info;
+	double pdf_all_light;
 	m_scene.SampleLight(light_surface_info, pdf_all_light);
-	//// check if light is blocked
-	//HitRecord block_rec;
-	//m_scene.HitHappened(Ray(hit_record.m_hit_position, obj2light), block_rec);
-
-	//Vector3 BRDF = hit_record.m_material->Eval(...);
-	//Vector3 kd(0.79, 0.76, 0.73); // temp diffuse white
-	Vector3 kd = hit_record.m_material.GetKd(); // temp diffuse white
-	Vector3 BRDF = kd / SimplePT::PI; // temp diffuse white
-
-	//Vector3 blocked_offset = block_rec.m_hit_position - hit_record.m_hit_position;
 
 	Vector3 dir2light = light_surface_info.m_hit_position - hit_record.m_hit_position;
 	Vector3 unit_dir2light = dir2light.Normalized();
-
+	Vector3 direct_illum_radiance;
 	if (m_Visible(hit_record.m_hit_position, light_surface_info.m_hit_position))
 	{
 		double r2 = dir2light.SquareLength();
@@ -144,7 +129,7 @@ Vector3 PathTracer::m_RayRadiance(const Ray& ray) const
 		out_radiance += direct_illum_radiance;
 	}
 
-	/* sampling exept light */
+	/* sampling no light */
 	// indir = brdf * (n * w) * m_RayRadiance(random_wi_on_hemisphere) / p  (integral on dwi)
 	Vector3 indirect_illum_radiance;
 	double RR = 0.8;
@@ -152,7 +137,6 @@ Vector3 PathTracer::m_RayRadiance(const Ray& ray) const
 	{
 		Vector3 wi;
 		double pdf_of_wi = -1.0;
-		//SimplePT::Sample_Hemisphere_Uniform(hit_record.m_hit_unit_normal, wi, pdf_of_wi);
 		SimplePT::Sample_Hemisphere_Cos_Weighted(hit_record.m_hit_unit_normal, wi, pdf_of_wi);
 		assert(pdf_of_wi > 0.0 && pdf_of_wi < 1.0);
 		Ray ray_wi(hit_record.m_hit_position, wi);
